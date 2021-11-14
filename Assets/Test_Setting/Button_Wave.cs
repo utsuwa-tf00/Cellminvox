6a9db059342b8953999875681a45acc581b480f2
Masaaki Nakata <masaaki.nakata@gmail.com>
2021-11-14 22:13:55 +0900

    unity更新

--- Assets/Test_Setting/Button_Wave.cs
+++ Assets/Test_Setting/Button_Wave.cs
@@ -7,27 +7,17 @@ public class Button_Wave : MonoBehaviour
 {
     public Text Wave;
 
+    public void Start()
+    {
+        Wave.text = "wave : " + Setting_GM.change_wave;
+    }
     public void OnClick()
     {
-        if(Setting_GM.change_wave == "sin")
-        {
-            Setting_GM.change_wave = "square";
-            Wave.text = "wave : square";
-        }
-        else if(Setting_GM.change_wave == "square")
-        {
-            Setting_GM.change_wave = "triangle";
-            Wave.text = "wave : triangle";
-        }
-        else if(Setting_GM.change_wave == "triangle")
-        {
-            Setting_GM.change_wave = "saw";
-            Wave.text = "wave : saw";
-        }
-        else if(Setting_GM.change_wave == "saw")
-        {
-            Setting_GM.change_wave = "sin";
-            Wave.text = "wave : sin";
-        }
+        if(Setting_GM.change_wave == "sin") Setting_GM.change_wave = "square";
+        else if(Setting_GM.change_wave == "square") Setting_GM.change_wave = "triangle";
+        else if(Setting_GM.change_wave == "triangle") Setting_GM.change_wave = "saw";
+        else if(Setting_GM.change_wave == "saw") Setting_GM.change_wave = "sin";
+
+        Wave.text = "wave : " + Setting_GM.change_wave;
     }
 }
